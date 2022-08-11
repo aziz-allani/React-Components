d1f00b212f166817495ff65ad98f5ddc532e4c65
aziz-allani <allani.aziz01@gmail.com>
2022-08-11 10:03:50 +0100

    Components Workshop

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Fullname from "./components/Fullname";
+import Adresse from "./components/Adresse";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Fullname />
+      <Adresse />
+    </div>
+  );
+}
+
+export default App;
